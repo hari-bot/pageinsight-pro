73fb1aed0807b7a3f27f2c871f4edb0e8392dccc
01XASSASSIN <hariharanuv7@gmail.com>
2024-07-09 13:39:09 +0530

    added icons in insights

--- src/components/InsightsDisplay.js
+++ src/components/InsightsDisplay.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUsers,
+  faThumbsUp,
+  faEye,
+  faChartLine,
+} from "@fortawesome/free-solid-svg-icons";
 
 const metricTitles = {
-  page_follows: "Total Followers / Fans",
-  page_post_engagements: "Total Engagement",
-  page_impressions: "Total Impressions",
-  page_actions_post_reactions_like_total: "Total Reactions",
+  page_follows: { title: "Total Followers / Fans", icon: faUsers },
+  page_post_engagements: { title: "Total Engagement", icon: faChartLine },
+  page_impressions: { title: "Total Impressions", icon: faEye },
+  page_actions_post_reactions_like_total: {
+    title: "Total Reactions",
+    icon: faThumbsUp,
+  },
 };
 
 const InsightsDisplayComponent = ({ insights }) => {
@@ -15,17 +25,28 @@ const InsightsDisplayComponent = ({ insights }) => {
         <p className="text-gray-500">Last 28 days</p>
       </div>
       <div className="grid grid-cols-2 gap-4">
-        {insights.map((insight, index) => (
-          <div
-            key={index}
-            className="border border-blue-300 p-5 rounded-lg shadow-sm"
-          >
-            <h3 className="text-lg font-semibold">
-              {metricTitles[insight.name] || "Unknown Metric"}
-            </h3>
-            <p className="text-xl font-semibold">{insight.values[0].value}</p>
-          </div>
-        ))}
+        {insights.map((insight, index) => {
+          const metric = metricTitles[insight.name] || {
+            title: "Unknown Metric",
+            icon: null,
+          };
+          return (
+            <div
+              key={index}
+              className="border border-blue-300 p-5 rounded-lg shadow-sm"
+            >
+              <h3 className="text-md font-semibold flex items-center mb-2">
+                {metric.icon && (
+                  <FontAwesomeIcon icon={metric.icon} className="mr-2" />
+                )}
+                {metric.title}
+              </h3>
+              <p className="text-2xl font-semibold">
+                {insight.values[0].value}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
