55db555dcf5029f480d6582aa090afcb07972032
hariharan <hariharan@mojowebtech.com>
2024-11-14 20:52:33 +0530

    Updated App Id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   useEffect(() => {
     window.fbAsyncInit = function () {
       window.FB.init({
-        appId: "1024624052426527",
+        appId: "385485799882497",
         cookie: true,
         xfbml: true,
         version: "v12.0",
